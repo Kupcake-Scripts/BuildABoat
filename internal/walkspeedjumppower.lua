f04900f323778dff88731521f6f88daf0c11fbe4
Matt <68496162+MattLawz@users.noreply.github.com>
2021-01-30 01:00:10 -0500

    Update walkspeedjumppower.lua

--- internal/walkspeedjumppower.lua
+++ internal/walkspeedjumppower.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game.HttpGet(game, 'https://pastebin.com/raw/vGwikY93'))()
 
-local w1 = library:Window('Walkspeed | Jumpspeed') -- Text
+local w1 = library:Window('Walkspeed | Jumppower') -- Text
 
 w1:Slider('WalkSpeed', 'WS', 16, 300, function(value)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
@@ -9,3 +9,11 @@ end)
 w1:Slider('JumpPower', 'JP', 50, 300, function(value)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
 end, 100) 
+
+w1:Button('Destroy GUI', function()
+    for i,v in pairs(game.CoreGui:GetChildren()) do
+        if v:FindFirstChild('Top') then
+            v:Destroy()    
+        end
+    end
+end)
