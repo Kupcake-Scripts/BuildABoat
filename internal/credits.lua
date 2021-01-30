39535d7486457f0033b9bbf3dbc9723dfd3ab62c
Matt <68496162+MattLawz@users.noreply.github.com>
2021-01-29 23:03:50 -0500

    Update credits.lua

--- internal/credits.lua
+++ internal/credits.lua
@@ -1,1 +1,7 @@
---// In Progress
+--// Thank you for using Kupcake Cheats scripts.
+
+--// Explore more on our below:
+
+--// https://toxicmods.mattlawz.dev
+
+--// Note: These credits will be checked if they are here, if not here the script will rewrite credits.
